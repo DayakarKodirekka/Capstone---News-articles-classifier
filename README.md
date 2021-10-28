bfa0c56d64d106b5dcfbea2e1460b9202899f013
Daya <dayakar.kodirekka@gmail.com>
2021-10-28 15:05:04 +0530

    Kafka and web application files

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 API key 0e797b4029ce46e6ad23858e5e7ac521 - newsapi.org
 
-https://www.datasciencelearner.com/insert-pandas-dataframe-into-mongodb/
-https://medium.com/analytics-vidhya/how-to-upload-a-pandas-dataframe-to-mongodb-ffa18c0953c1
+
---
+++ kafka/keys.py
@@ -0,0 +1,4 @@
+# put your API keys here
+mongodb_connection_string = "mongodb+srv://dayakar:3292%40wtt@clustertest.lgfcs.mongodb.net/newsDb?retryWrites=true&w=majority"
+rapidapi_key = "ffae7faa19mshe3e58b4b54701cap1cb09djsn8af8566f33a3"
+newscather_key = ""
