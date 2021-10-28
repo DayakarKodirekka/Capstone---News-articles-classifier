e7cfdbfe39384c392d0965cecc0ca32f551cacbd
Daya <dayakar.kodirekka@gmail.com>
2021-10-28 15:56:54 +0530

    password revoked

--- kafka/keys.py
+++ kafka/keys.py
@@ -1,4 +1,5 @@
 # put your API keys here
-mongodb_connection_string = "mongodb+srv://dayakar:3292%40wtt@clustertest.lgfcs.mongodb.net/newsDb?retryWrites=true&w=majority"
+mongodb_connection_string = "mongodb+srv://dayakar:password@clustertest.lgfcs.mongodb.net/newsDb?retryWrites=true&w=majority"
 rapidapi_key = "ffae7faa19mshe3e58b4b54701cap1cb09djsn8af8566f33a3"
 newscather_key = ""
+# 
